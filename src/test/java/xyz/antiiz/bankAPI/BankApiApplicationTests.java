b751a02d316e88892a5a8826070af3f3c9d374ba
theantiz <jaychothiyawala04@gmail.com>
2026-01-07 06:39:09 +0530

    Initial Commit

---
+++ src/test/java/xyz/antiiz/bankAPI/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.antiiz.bankAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
